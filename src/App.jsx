b7bdbef525ca23f73d3c720605ed272c1780bbc6
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2026-01-13 15:46:28 +0100

    Feat: Set up routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import "./styles/App.css";
+import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./styles/App.css";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
